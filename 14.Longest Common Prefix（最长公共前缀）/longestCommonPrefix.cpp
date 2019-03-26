d82b051be3e67ce0d52f5dc1377bd924c37b5802
anthonyzhi <1373720172@qq.com>
2019-03-26 22:16:15 +0800

    Update longestCommonPrefix.cpp

--- 14.Longest Common Prefix（最长公共前缀）/longestCommonPrefix.cpp
+++ 14.Longest Common Prefix（最长公共前缀）/longestCommonPrefix.cpp
@@ -3,6 +3,8 @@
 //如果不存在公共前缀，返回空字符串 ""。
 
 
+//方法一   LCP(S 1 …S n  )=LCP(LCP(LCP(S 1  ,S 2  ),S 3  ),…S n  )
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
@@ -18,5 +20,27 @@ public:
             }
         }
         return prevstr;
+    }
+};
+
+
+
+
+
+//方法二我们从前往后枚举字符串的每一列，先比较每个字符串相同列上的字符（即不同字符串相同下标的字符）然后再进行对下一列的比较。
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0)
+            return "";
+        for(int i=0;i<strs[0].size();i++){
+            char c=strs[0][i];
+            for(int j=1;j<strs.size();j++){
+                if(i==strs[j].size()||strs[j][i]!=c)
+                    return strs[0].substr(0,i);
+            }
+        }
+        return strs[0];
     }
 };
