e59e4fa9cea76ad33319c521a2b400e90f167d4d
anthonyzhi <1373720172@qq.com>
2019-03-31 20:55:25 +0800

    Update palindromeLinkedList.cpp

--- 234. Palindrome Linked List(回文链表）/palindromeLinkedList.cpp
+++ 234. Palindrome Linked List(回文链表）/palindromeLinkedList.cpp
@@ -25,3 +25,36 @@ public:
         return isEqual(head,h);
     }
 };
+
+
+
+
+//方法二
+
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow=head, *fast=head,*prev=nullptr;
+        //使用快慢指针找到链表中点
+        while(fast){
+            slow=slow->next;
+            fast=fast->next?fast->next->next:fast->next;            
+        }
+        //reverse逆序后半部分
+        while(slow){
+            ListNode* ovn=slow->next;
+            slow->next=prev;
+            prev=slow;
+            slow=ovn;
+        }
+        //从头、中点开始比较是否相同
+        while(head&&prev){
+            if(head->val!=prev->val)
+                return false;
+            head=head->next;
+            prev=prev->next;
+        }
+        return true;
+    }
+};
