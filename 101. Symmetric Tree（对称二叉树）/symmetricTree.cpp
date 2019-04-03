72f5dfb31e475eb995a2cb74feb816898c4d9835
anthonyzhi <1373720172@qq.com>
2019-04-03 16:44:05 +0800

    Update symmetricTree.cpp

--- 101. Symmetric Tree（对称二叉树）/symmetricTree.cpp
+++ 101. Symmetric Tree（对称二叉树）/symmetricTree.cpp
@@ -25,3 +25,31 @@ public:
         return false;
     }
 };
+
+
+
+//方法二  在对列中使用迭代
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<TreeNode *>q;
+        q.push(root);
+        q.push(root);
+        while(!q.empty()){
+            TreeNode *t1=q.front();q.pop();    
+            TreeNode *t2=q.front();q.pop();
+            if(t1==NULL&&t2==NULL)
+                continue;
+            if(t1==NULL||t2==NULL)
+                return false;
+            if(t1->val!=t2->val)
+                return false;
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
+    }
+};
