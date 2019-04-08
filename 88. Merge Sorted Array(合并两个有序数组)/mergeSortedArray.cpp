840fd3eca4d1f6a8e9496621f3644ddf0313d175
anthonyzhi <1373720172@qq.com>
2019-04-08 16:58:51 +0800

    Update mergeSortedArray.cpp

--- 88. Merge Sorted Array(合并两个有序数组)/mergeSortedArray.cpp
+++ 88. Merge Sorted Array(合并两个有序数组)/mergeSortedArray.cpp
@@ -9,3 +9,17 @@ public:
         sort(nums1.begin(),nums1.end());
     }
 };
+
+
+
+//法二：
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p=m-- + n-- - 1;
+        while(m>=0&&n>=0)
+            nums1[p--]=nums1[m]>nums2[n]?nums1[m--]:nums2[n--];
+        while(n>=0)
+            nums1[p--]=nums2[n--];
+    }
+};
