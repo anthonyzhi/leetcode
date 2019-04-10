79bdb479e0e39aebdc332695167f1af90bf03932
anthonyzhi <1373720172@qq.com>
2019-04-10 16:08:23 +0800

    Update houseRobber.cpp

--- 198. House Robber（打家劫舍）/houseRobber.cpp
+++ 198. House Robber（打家劫舍）/houseRobber.cpp
@@ -11,3 +11,23 @@ public:
         return even>odd?even:odd;
     }
 };
+
+
+//动态规划  
+//动态规划，计算前 i 家的最大值时，考虑第 i 家 偷与不偷。
+//如果偷第 i 家，则为第 i 家加上 前 i-2 家最大值。如果不偷第 i 家，则直接返回 前 i - 1 家最大值。
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        int dp[nums.size()+1];
+        dp[0]=0;
+        dp[1]=max(dp[0], nums[0]);
+        for(int i=2;i<=nums.size();i++){
+            dp[i]=max(dp[i-1], dp[i-2]+nums[i-1]);
+        }
+        return dp[nums.size()];
+    }
+};
