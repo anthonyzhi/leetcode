5c11bb214f167485bd0340279312395b742be0a0
anthonyzhi <1373720172@qq.com>
2019-04-14 23:20:34 +0800

    Update romanToInt.cpp

--- 13. Roman to Integer（罗马数字转整数）/romanToInt.cpp
+++ 13. Roman to Integer（罗马数字转整数）/romanToInt.cpp
@@ -1,4 +1,4 @@
-//法一  
+
  /*
         1、从后往前看，先将当前字符转换为对应数值，再判断当前数值是否大于它的前一个数值；
         2、若大于等于它的前一个数值，那么加上该值
@@ -33,24 +33,3 @@ public:
     }
 };
 
-
-//法二
-
-class Solution {
-public:
-    int romanToInt(string s) {
-       unordered_map<char,int>map{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100}, {'D',500},{'M',1000},};
-        int j=1,ans=0;
-        for(int i=0;i<s.size();i++){
-            if(map[s[i]]>=map[s[j]]){
-                ans+=map[s[i]];
-                j++;
-            }
-            else{
-                ans+=map[s[j++]]-map[s[i++]];
-                j++;
-            }
-        }
-        return ans;
-    }
-};
