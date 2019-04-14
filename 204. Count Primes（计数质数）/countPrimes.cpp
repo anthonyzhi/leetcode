3c711008d34e53c43d058cdea622adc48a42f237
anthonyzhi <1373720172@qq.com>
2019-04-14 21:58:05 +0800

    Update countPrimes.cpp

--- 204. Count Primes（计数质数）/countPrimes.cpp
+++ 204. Count Primes（计数质数）/countPrimes.cpp
@@ -3,19 +3,18 @@ public:
     int countPrimes(int n) {
         if(n<3)
             return 0;
-        int res[n]={1};
-        res[1]=1;
+        vector<int>res(n,1);
+        res[0]=res[1]=0;
         for(int i=2;i<sqrt(n)+1;i++){
-           if(res[i]==0){
-               for(int j=2*i;j<n;j=j+i)
-                   res[j]=1;
+           if(res[i]==1){
+               for(int j=i*i;j<n;j=j+i)
+                   res[j]=0;
            } 
         }
-        int sum=0;
+        int ans=0;
         for(int i=0;i<n;i++){
-            if(res[i]==0)
-                sum+=1;
+           ans+=res[i];
         }
-        return sum;
+        return ans;
     }
 };
