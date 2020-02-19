18b5b758d81d7c983a160c2ed617d92eb5fd5011
anthonyzhi <1373720172@qq.com>
2020-02-19 22:55:39 +0800

    Update KthSmallestElementinaBST.java

--- 230. Kth Smallest Element in a BST/KthSmallestElementinaBST.java
+++ 230. Kth Smallest Element in a BST/KthSmallestElementinaBST.java
@@ -26,3 +26,37 @@ class Solution {
         return 1+nodeCount(root.left)+nodeCount(root.right);
     }
 }
+
+
+
+
+//方法二
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int num=0;
+    int res;
+    public int kthSmallest(TreeNode root, int k) {
+        inorderTraversal(root,k);
+        return res;
+    }
+
+    private void inorderTraversal(TreeNode node, int k){
+        if(node==null) return;
+        inorderTraversal(node.left,k);
+        num++;
+        if(num == k){
+            res=node.val;
+            return;
+        }
+        inorderTraversal(node.right,k);
+    }
+}
